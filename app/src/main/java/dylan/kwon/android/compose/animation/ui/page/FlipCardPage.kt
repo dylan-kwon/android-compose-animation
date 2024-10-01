3d7ddb3fc00eac0ce701999042067064b56a302a
dylan-kwon <dylan.kwon98@gmail.com>
2024-10-01 23:20:11 +0900

    FlipCardPage에서 Scaffold 제거

--- app/src/main/java/dylan/kwon/android/compose/animation/ui/page/FlipCardPage.kt
+++ app/src/main/java/dylan/kwon/android/compose/animation/ui/page/FlipCardPage.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,30 +17,24 @@ import dylan.kwon.android.compose.animation.ui.theme.ComposeanimationTheme
 
 @Composable
 fun FlipCardPage() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Box(
-            modifier = Modifier.padding(it),
+    Box {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                FlipCard(
-                    modifier = Modifier
-                        .padding(horizontal = 50.dp)
-                        .fillMaxWidth()
-                        .aspectRatio(3 / 4f)
-                )
-            }
+            FlipCard(
+                modifier = Modifier
+                    .padding(horizontal = 50.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(3 / 4f)
+            )
         }
     }
 }
 
-@Preview
 @Composable
+@Preview(showBackground = true)
 fun FlipCardPagePreview() {
     ComposeanimationTheme {
         FlipCardPage()
